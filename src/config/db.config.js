9678dc16bd0b450f780edbdb9c1bad09aaeccac6
rahemanali <sales@junkiescoder.com>
2024-12-21 16:42:40 +0530

    added vercel file

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { MONGODB_URI } = require("./server.config");
 
 const connectToDB = async () => {
-  const connectionInstance = await mongoose.connect(`${MONGODB_URI}`);
+  const connectionInstance = mongoose.connect(`${MONGODB_URI}`);
   console.log(
     `\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
   );
