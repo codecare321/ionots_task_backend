ff2202a7329d1411cccfd4b95705b3a114450064
rahemanali <sales@junkiescoder.com>
2024-12-21 16:52:40 +0530

    added vercel file

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,10 +2,19 @@ const mongoose = require("mongoose");
 const { MONGODB_URI } = require("./server.config");
 
 const connectToDB = async () => {
-  const connectionInstance = mongoose.connect(`${MONGODB_URI}`);
-  console.log(
-    `\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
-  );
+  try {
+    const connectionInstance = await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      poolSize: 10,
+    });
+    console.log(
+      `\nMongoDB connected successfully! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit the process if the DB connection fails
+  }
 };
 
 module.exports = connectToDB;
