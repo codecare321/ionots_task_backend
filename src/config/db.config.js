95c3a76c0db377e17ac7c158aaf8fe54c76e9942
rahemanali <sales@junkiescoder.com>
2024-12-21 16:55:12 +0530

    added vercel file

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,18 +2,33 @@ const mongoose = require("mongoose");
 const { MONGODB_URI } = require("./server.config");
 
 const connectToDB = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      poolSize: 10,
-    });
-    console.log(
-      `\nMongoDB connected successfully! DB HOST: ${connectionInstance.connection.host}`
-    );
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-    process.exit(1); // Exit the process if the DB connection fails
+  const MAX_RETRIES = 5;
+  const RETRY_DELAY = 2000; // 2 seconds
+
+  for (let attempt = 1; attempt <= MAX_RETRIES; attempt++) {
+    try {
+      const connectionInstance = await mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: 10000, // 10 seconds timeout
+      });
+      console.log(
+        `MongoDB connected! Host: ${connectionInstance.connection.host}`
+      );
+      return;
+    } catch (error) {
+      console.error(
+        `Attempt ${attempt} - Error connecting to MongoDB:`,
+        error.message
+      );
+      if (attempt < MAX_RETRIES) {
+        console.log(`Retrying in ${RETRY_DELAY / 1000} seconds...`);
+        await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
+      } else {
+        console.error("Max retries reached. Exiting...");
+        process.exit(1);
+      }
+    }
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const corsConfig = {
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
-app.options("", cors(corsConfig));
+app.options("*", cors(corsConfig));
 app.use(cors(corsConfig));
 
 app.use("/api", projectRoutes);
